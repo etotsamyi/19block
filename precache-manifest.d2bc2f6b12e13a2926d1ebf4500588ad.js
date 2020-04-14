62f77cdcc879a0ce7506a8df59932b9bb119d4ad
etotsamyi <51693784+etotsamyi@users.noreply.github.com>
2020-04-15 01:57:36 +0300

    Updates

---
+++ precache-manifest.d2bc2f6b12e13a2926d1ebf4500588ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e12c0be36099f907dab4ba8f22fccc",
+    "url": "/19block/index.html"
+  },
+  {
+    "revision": "083257c88b9264c875d0",
+    "url": "/19block/static/css/main.fa7a10e5.chunk.css"
+  },
+  {
+    "revision": "3744a6c150ede5c8b784",
+    "url": "/19block/static/js/2.ebccd86e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/19block/static/js/2.ebccd86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "083257c88b9264c875d0",
+    "url": "/19block/static/js/main.75deffe0.chunk.js"
+  },
+  {
+    "revision": "0103ab1de174c37b5741",
+    "url": "/19block/static/js/runtime-main.5c84a533.js"
+  }
+]);
